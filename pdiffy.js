ae630fee9ccf654bbe37f7783530ca29f99bf96e
damoeb <markus@migor.org>
2017-07-28 01:56:55 +0200

    first commit

---
+++ pdiffy.js
@@ -0,0 +1,3 @@
+const pdiffy = (tests) => {
+  tests();
+};
---
+++ tests/example.spec.js
@@ -0,0 +1,9 @@
+console.log(pdiffy);
+
+pdiffy(() => {
+  describe('example test', function() {
+    it('should be true', function() {
+      expect('foo').toBe('foo');
+    });
+  });
+});
