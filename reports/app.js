804cc5a677659f786e3ccb09a905ecc32b79aae4
Markus Ruepp <mruepp@extron.com>
2017-07-31 13:42:17 +0200

    filter option

--- reports/app.js
+++ reports/app.js
@@ -2,7 +2,15 @@ angular.module('pdiffy', [])
   .controller('ReportCtrl', function($scope, $http) {
   $http.get('pdiffy-report.json')
     .then(function(res){
-      $scope.specsCount = res.data.length;
-      $scope.failedSpecs = res.data.filter(spec => !spec.passed);
+      const allSpecs = res.data;
+      $scope.failedOnly = true;
+      $scope.specsCount = allSpecs.length;
+      $scope.getSpecs = () => {
+        if ($scope.failedOnly) {
+          return allSpecs.filter(spec => !spec.passed);
+        } else {
+          return allSpecs;
+        }
+      }
     });
 });
